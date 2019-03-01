7395a1b872590ff5513e584e1b0d10c4f7b8edd5
barreUC3M <100330734@alumnos.uc3m.es>
2019-03-01 11:52:36 +0100

    Modify converter for interpolation mode.

--- utils/voc/converter_interpolation.py
+++ utils/voc/converter_interpolation.py
@@ -9,6 +9,11 @@ directory.
 This implementation only supports bounding boxes in CVAT annotation format, and
 warns if it encounter any tracks or annotations that are not bounding boxes,
 ignoring them in both cases.
+
+
+Created on Tue Feb 19 15:12:58 2019
+
+@author: jmsamblas, dbarrejon
 """
 
 import os
@@ -28,6 +33,11 @@ def parse_args():
         '--cvat-xml', metavar='FILE', required=True,
         help='input file with CVAT annotation in xml format. Only the file, not full path.'
     )
+    
+    parser.add_argument(
+        '--video', metavar='FILE', required=True,
+        help='Video file for the annotations. Only the file, not full path.'
+    )
 
     parser.add_argument(
         '--output-dir', metavar='DIRECTORY', required=True,
@@ -37,7 +47,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def process_cvat_xml(xml_file, output_dir):
+def process_cvat_xml(xml_file, output_dir, video):
     """
     Transforms a single XML in CVAT format to multiple PASCAL VOC format
     XMls.
@@ -51,15 +61,16 @@ def process_cvat_xml(xml_file, output_dir):
     cvat_xml = etree.parse(xml_file)
 
     basename = os.path.splitext( os.path.basename( xml_file ) )[0]
-    video_name = basename + '.MP4'
+    video_name = os.path.basename(video)
+    # video_name = basename + '.MP4'
     tracks = cvat_xml.findall( './/track' )
     #print( "Tracks: "
 
-    # Build up a list of each bounding box per frame
+    # Build up a dictionary of each bounding box per frame
 
     frames = {}
     
-    # Parse xml and gather data
+    #%% Parse xml and gather data
     for track in tracks:
         trackid = int(track.get("id"))
         label = track.get("label")
@@ -80,40 +91,43 @@ def process_cvat_xml(xml_file, output_dir):
                 frame[ trackid ] = { 'xtl': xtl, 'ytl': ytl, 'xbr': xbr, 'ybr': ybr, 'label': label }
                 frames[ frameid ] = frame
 
-    # Get rid of the frames without hitos
+    #%% Get rid of the frames without hitos
     for frameid in sorted(frames.keys()):
-        
+
         frame = frames[frameid]
         objids = sorted(frame.keys()) # track ids
         hito_present = 0
 
         for objid in objids:
             obj = frame[objid]
-            if 'hito_right' in obj.values() | 'hito_left' in obj.values():
+            if 'hito_right' in obj.values() or  'hito_left' in obj.values():
                 hito_present = 1
-        
-        if not hito_present:
+                break             
+        if not hito_present :
             del frames[frameid] # Delete frame without hitos
             
             
-
-   # Generate Data set
-    video_absPath = os.path.join(output_dir, video_name)
+   #%% Generate Data set Images
+    video_absPath = os.path.abspath(video)
     anno_dir = os.path.join(os.path.expanduser(output_dir), basename)
     os.makedirs(anno_dir, exist_ok=True)
     
     generate_dataset(video_absPath, anno_dir, frames )
 
-    # Spit out a list of each object for each frame
+    #%% Spit out a list of each object for each frame
+    
+    width = int(cvat_xml.find('.//original_size/width').text)
+    height  = int(cvat_xml.find('.//original_size/height').text)
+    
     for frameid in sorted(frames.keys()):
         print( frameid )
 
         image_name = "%s_%05d.jpg" % (basename, frameid)
-        original_size = cvat_xml.findall('.//original_size')[0]
-        elem_width = original_size.getchildren()[0]
-        width = int(elem_width.text)
-        elem_height = original_size.getchildren()[1]
-        height = int(elem_height.text)
+#        original_size = cvat_xml.findall('.//original_size')[0]
+#        elem_width = original_size.getchildren()[0]
+#        width = int(elem_width.text)
+#        elem_height = original_size.getchildren()[1]
+#        height = int(elem_height.text)
 
         image_path = os.path.join(anno_dir, image_name)
         if not os.path.exists(image_path):
@@ -129,13 +143,9 @@ def process_cvat_xml(xml_file, output_dir):
 
         for objid in objids:
 
-            obj = frame[objid]
-            print( "    %d: (%f,%f) (%f,%f) '%s'" % (objid, obj['xtl'],obj['ytl'],obj['xbr'],obj['ybr'],obj['label']) )
-
-
-        for objid in objids:
-
             box = frame[objid]
+            
+            print( "    %d: (%f,%f) (%f,%f) '%s'" % (objid, box['xtl'],box['ytl'],box['xbr'],box['ybr'],box['label']) )
 
             label = box.get('label')
             xmin = float(box.get('xtl'))
@@ -161,13 +171,13 @@ def generate_dataset(video_absPath, anno_dir, frames):
     :param anno_dir: directory of annotations with PASCAL VOC format
     :frames: dictionary with the annotated frames infomrmation
     """
-    [video_dir, video_name]  =os.path.split(video_absPath)
     basename = os.path.splitext( os.path.basename( video_absPath ) )[0]
     cap = cv2.VideoCapture(video_absPath)
     list_frameId = [*frames]
     
+    ## Faster way
     while (cap.isOpened()): # open video file
-        
+    
         frameId = int(cap.get(1)) #current frame number
         ret, frame = cap.read()
         
@@ -181,10 +191,23 @@ def generate_dataset(video_absPath, anno_dir, frames):
                 cap.release()
                 print('Dataset generated at ' + anno_dir)
                 return
+        
+#    ## Slower way   
+#    for frameid in list_frameId:
+#        cap.set(1, frameid)
+#        ret, frame = cap.read()
+#        
+#        framename = "%s_%05d.jpg" % (basename, frameid)
+#        filename = os.path.join(anno_dir, framename)
+#        cv2.imwrite(filename, frame) # store annotated frame
+#                    
+#    cap.release()
+#    print('Dataset generated at ' + anno_dir)
+
             
 def main():
     args = parse_args()
-    frames = process_cvat_xml(args.cvat_xml, args.output_dir)
+    frames = process_cvat_xml(args.cvat_xml, args.output_dir,args.video)
 
 
 if __name__ == "__main__":
